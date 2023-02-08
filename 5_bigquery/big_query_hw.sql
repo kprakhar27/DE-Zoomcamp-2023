57374dbab5595492ba74d1ed6c4d217e83a9a02f
kprakhar27 <kumarprkhr@gmail.com>
2023-02-08 11:16:16 +0530

    big query homework

--- 5_bigquery/big_query_hw.sql
+++ 5_bigquery/big_query_hw.sql
@@ -1,40 +1,36 @@
 -- Create a external table for for-hire vehicles for the year 2019
-CREATE OR REPLACE EXTERNAL TABLE `data-engineering-369507.trips_data_all.fhv_tripdata`
+CREATE OR REPLACE EXTERNAL TABLE `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['gs://dtc_data_lake_data-engineering-369507/raw/fhv_tripdata/2019/fhv_tripdata_2019-*.parquet']
+  uris = ['gs://dtc_data_lake_de-zoomcamp-2023-375817/raw/fhv_tripdata/2019/fhv_tripdata_2019-*.parquet']
 );
 
--- Q1 What is count for fhv vehicles data for year 2019
-SELECT COUNT(*) FROM `data-engineering-369507.trips_data_all.fhv_tripdata`;
-
--- Q2 How many distinct dispatching_base_num we have in fhv for 2019
-SELECT COUNT(DISTINCT(dispatching_base_num)) FROM `data-engineering-369507.trips_data_all.fhv_tripdata`;
+-- Q1 What is the count for fhv vehicle records for year 2019?
+SELECT COUNT(*) FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`;
 
 -- Create a nonpartitioned table for for-hire vehicles for the year 2019
-CREATE OR REPLACE TABLE `data-engineering-369507.trips_data_all.fhv_nonpartitioned_tripdata`
-AS SELECT
-  CAST(dispatching_base_num AS STRING) AS dispatching_base_num,
-  CAST(pickup_datetime AS TIMESTAMP) AS pickup_datetime,
-  CAST(dropOff_datetime AS TIMESTAMP) AS dropOff_datetime,
-  CAST(PUlocationID AS FLOAT64) AS PUlocationID,
-  CAST(DOlocationID AS FLOAT64) AS DOlocationID,
-  -- CAST(SR_Flag AS INT64) AS SR_Flag,
-  CAST(Affiliated_base_number AS STRING) AS Affiliated_base_number
-FROM `data-engineering-369507.trips_data_all.fhv_tripdata`;
+CREATE OR REPLACE TABLE `de-zoomcamp-2023-375817.trips_data_all.fhv_nonpartitioned_tripdata`
+AS SELECT * FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`;
 
--- Q3 Best strategy to optimise if query always filter by dropoff_datetime and order by dispatching_base_num
-CREATE OR REPLACE TABLE `data-engineering-369507.trips_data_all.fhv_partitioned_tripdata`
-PARTITION BY DATE(dropoff_datetime)
-CLUSTER BY dispatching_base_num AS (
-  SELECT * FROM `data-engineering-369507.trips_data_all.fhv_tripdata`
+-- Q2 How many distinct Affiliated_base_number we have in fhv for 2019?
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`;
+
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_nonpartitioned_tripdata`;
+
+-- Q3 How many records have both a blank (null) PUlocationID and DOlocationID in the entire dataset?
+SELECT COUNT(*) FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`
+WHERE `PUlocationID`is NULL AND `DOlocationID`is NULL;
+
+-- Q4 What is the best strategy to optimize the table if query always filter by pickup_datetime and order by affiliated_base_number?
+CREATE OR REPLACE TABLE `de-zoomcamp-2023-375817.trips_data_all.fhv_partitioned_tripdata`
+PARTITION BY DATE(pickup_datetime)
+CLUSTER BY Affiliated_base_number AS (
+  SELECT * FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_tripdata`
 );
 
-SELECT count(*) FROM  `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+-- Q5 Write a query to retrieve the distinct affiliated_base_number between pickup_datetime 03/01/2019 and 03/31/2019 (inclusive)
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM  `de-zoomcamp-2023-375817.trips_data_all.fhv_nonpartitioned_tripdata`
+WHERE pickup_datetime BETWEEN '2019-03-01' AND '2019-03-31';
 
-
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `de-zoomcamp-2023-375817.trips_data_all.fhv_partitioned_tripdata`
+WHERE pickup_datetime BETWEEN '2019-03-01' AND '2019-03-31';
